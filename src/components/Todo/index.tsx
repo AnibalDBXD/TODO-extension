abc959f061a3a3cf7a6039ee75d8e2803fe67aba
AnibalDBXD <olivaresluisanibal@gmail.com>
2021-06-09 18:04:01 -0400

    Create todo header

--- src/components/Todo/index.tsx
+++ src/components/Todo/index.tsx
@@ -1,12 +1,18 @@
 import React from 'react';
+import Switch from '../Switch';
 import List from './List';
+import "./index.scss"
 
 const Todo = () => {
-  return (
-    <div>
-      <List />
-    </div>
-  );
+    return (
+        <div>
+            <div className="header">
+                <h1>Todo-List</h1>
+                <Switch />
+            </div>
+            <List />
+        </div>
+    );
 };
 
 export default Todo;
