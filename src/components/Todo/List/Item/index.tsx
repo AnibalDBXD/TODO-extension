627f4f3e6060525cfd027b171a2f9ce285a311f3
AnibalDBXD <olivaresluisanibal@gmail.com>
2021-06-09 17:40:24 -0400

    npm run prettier  and Create Popup components

---
+++ src/components/Todo/List/Item/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Item = () => {
+  return (
+    <li>
+      <p>Comida</p>
+      <button>Check</button>
+      <button>Delete</button>
+    </li>
+  );
+};
+
+export default Item;
---
+++ src/pages/Popup/components/Popup/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import Todo from '../../../../components/Todo';
+import './Popup.css';
+
+const Popup = () => {
+  return <Todo />;
+};
+
+export default Popup;
