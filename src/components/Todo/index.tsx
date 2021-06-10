1bbe9a4d7e8798c04f22d70c0f3961dd9998ed52
AnibalDBXD <olivaresluisanibal@gmail.com>
2021-06-10 16:12:58 -0400

    Search and Create Input

--- src/components/Todo/index.tsx
+++ src/components/Todo/index.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import Switch from '../Switch';
 import List from './List';
 import "./index.scss";
+import Input from '../Input';
 
 const Todo = () => {
     return (
@@ -9,6 +10,10 @@ const Todo = () => {
             <div className="header">
                 <h1>Todo-List</h1>
                 <Switch />
+            </div>
+            <div className="InputContainer">
+                <Input placeholder="Search" />
+                <Input placeholder="Create" />
             </div>
             <List />
         </div>
